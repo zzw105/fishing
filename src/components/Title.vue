d4e31ec901a9fef1b9f4735e20a249bdf40c9aae
张泽文 <13611782678@163.com>
2022-07-02 15:00:26 +0800

    完成首页

---
+++ src/components/Title.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="title">title</div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Vue logo" src="@/assets/svg/fishingLogo.svg" />
+    <el-button size="large" type="primary">开始钓鱼</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 });
 </script>
+
+<style lang="less" scoped>
+.home {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+  height: 360px;
+  border-radius: 5%;
+  background-color: #eaf1ff;
+  box-shadow: 3px -3px 4px #aaaaaa;
+  margin: auto;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+
+  img {
+    width: 300px;
+    height: 300px;
+    margin-bottom: 10px;
+  }
+}
+</style>
